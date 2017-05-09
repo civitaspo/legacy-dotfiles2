8896ed220068d30ce34c43e403a9198274c44206
Civitaspo <civitaspo@gmail.com>
2017-05-09 10:21:19 +0900

    Add debug mode

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+DEBUG="-l debug"
+if [ -z "$1" ]; then
+  DEBUG=""
+fi
 
 bin/setup
 
 # Homebrew does not allow sudo.
 case "$(uname)" in
-  "Darwin")  bin/mitamae local lib/recipe.rb ;;
-  *) sudo -E bin/mitamae local lib/recipe.rb ;;
+  "Darwin")  bin/mitamae local lib/recipe.rb $DEBUG ;;
+  *) sudo -E bin/mitamae local lib/recipe.rb $DUBUG ;;
 esac
