e7957e065fcb76a3e4ab79141793e45303cad26b
Civitaspo <civitaspo@gmail.com>
2017-06-16 14:17:33 +0900

    Solve key repeat bugs

--- config/.hammerspoon/init.lua
+++ config/.hammerspoon/init.lua
@@ -17,7 +17,7 @@ local function keyCode(key, modifiers)
   modifiers = modifiers or {}
   return function()
     hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-    hs.timer.usleep(1000)
+    -- hs.timer.usleep(1000)
     hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
     -- hs.eventtap.keyStroke(modifiers, string.lower(key), 1)
   end
