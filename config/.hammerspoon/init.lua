adf846746fba2e9c3342df6d751ddb1f6b4bbdf0
Civitaspo <civitaspo@gmail.com>
2017-08-30 03:06:12 +0900

    HammerSpoon: Add Continuous Writing Support ref. http://blog.gururimichi.com/entry/2017/08/28/190030

--- config/.hammerspoon/init.lua
+++ config/.hammerspoon/init.lua
@@ -15,4 +15,78 @@ do
   pathWatcher:start()
 end
 
+--
+-- ContinuousWriting Support
+--   ref. http://blog.gururimichi.com/entry/2017/08/28/190030
+--
 
+do
+  local noStrokeSconds = 0
+  local noStrokeCheckInterval = 30
+  local isStroked = true
+  local timer = hs.timer.doEvery(noStrokeCheckInterval, function()
+    if isStroked then
+      noStrokeSconds = 0
+      isStroked = false
+    else
+      noStrokeSconds = noStrokeSconds + noStrokeCheckInterval
+      hs.notify.show("Hammerspoon", "No Key Stroke Event", "During " .. noStrokeSconds .." seconds"):send()
+    end
+  end)
+  local eventtap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+    isStroked = true
+    return false
+  end)
+  local function startContinuousWriting()
+    if not timer:running() then
+      timer:start()
+    end
+    if not eventtap:isEnabled() then
+      eventtap:start()
+    end
+  end
+  local function stopContinuousWriting()
+    if timer:running() then
+      timer:stop()
+    end
+    if eventtap:isEnabled() then
+      eventtap:stop()
+    end
+  end
+  caffeinateWatcher = hs.caffeinate.watcher.new(function(eventType)
+    if eventType == hs.caffeinate.watcher.screensaverDidStart then
+      print("-- stopContinuousWriting: hs.caffeinate.watcher.screensaverDidStart")
+      stopNonStopWriting()
+    elseif eventType == hs.caffeinate.watcher.screensaverDidStop then
+      print("-- startNonStopWriting: hs.caffeinate.watcher.screensaverDidStop")
+      startNonStopWriting()
+    elseif eventType == hs.caffeinate.watcher.screensaverWillStop then
+      print("-- stopContinuousWriting: hs.caffeinate.watcher.screensaverWillStop")
+      stopContinuousWriting()
+    elseif eventType == hs.caffeinate.watcher.screensDidLock then
+      print("-- stopContinuousWriting: hs.caffeinate.watcher.screensDidLock")
+      stopContinuousWriting()
+    elseif eventType == hs.caffeinate.watcher.screensDidSleep then
+      print("-- stopContinuousWriting: hs.caffeinate.watcher.screensDidSleep")
+      stopContinuousWriting()
+    elseif eventType == hs.caffeinate.watcher.screensDidUnlock then
+      print("-- startContinuousWriting: hs.caffeinate.watcher.screensDidUnlock")
+      startContinuousWriting()
+    elseif eventType == hs.caffeinate.watcher.screensDidWake then
+      print("-- startContinuousWriting: hs.caffeinate.watcher.screensDidWake")
+      startContinuousWriting()
+    elseif eventType == hs.caffeinate.watcher.sessionDidBecomeActive then
+      print("-- startContinuousWriting: hs.caffeinate.watcher.sessionDidBecomeActive")
+      startContinuousWriting()
+    elseif eventType == hs.caffeinate.watcher.sessionDidResignActive then
+      print("-- startContinuousWriting: hs.caffeinate.watcher.sessionDidResignActive")
+      startContinuousWriting()
+    else
+      print("-- Do nothing about ContinuousWriting: " .. eventType)
+    end
+  end)
+  startContinuousWriting()
+  caffeinateWatcher:start()
+end
+
+--
