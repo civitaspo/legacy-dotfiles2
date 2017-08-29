816dcc89a63c9520d281bc778bf0f6d4f43f2812
Civitaspo <civitaspo@gmail.com>
2017-08-30 03:05:43 +0900

    HammerSpoon: Define a scope for config reloader

--- config/.hammerspoon/init.lua
+++ config/.hammerspoon/init.lua
@@ -1,16 +1,18 @@
-function reloadConfig(files)
-  doReload = false
-  for _,file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-      print("-- Loading config: "..file)
+do
+  local function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+      if file:sub(-4) == ".lua" then
+        doReload = true
+        print("-- Loading config: "..file)
+      end
+    end
+    if doReload then
+      hs.reload()
     end
   end
-  if doReload then
-    hs.reload()
-  end
+  local pathWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
+  pathWatcher:start()
 end
-local pathWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
-pathWatcher:start()
 
 
