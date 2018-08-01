fa9211999308d3b7c57bd1c5ab15c30940aa8692
Civitaspo <civitaspo@gmail.com>
2018-08-01 15:04:24 +0900

    Use gls

--- config/.zsh/20_aliases.zsh
+++ config/.zsh/20_aliases.zsh
@@ -1,15 +1,15 @@
 alias ..='cd ..'
-alias ld='\ls -ld'          # Show info about the directory
-alias lla='\ls -lAF'        # Show hidden all files
-alias ll='\ls -lF'          # Show long file information
-alias la='\ls -AF'          # Show hidden files
-alias lx='\ls -lXB'         # Sort by extension
-alias lk='\ls -lSr'         # Sort by size, biggest last
-alias lc='\ls -ltcr'        # Sort by and show change time, most recent last
-alias lu='\ls -ltur'        # Sort by and show access time, most recent last
-alias lt='\ls -ltr'         # Sort by date, most recent last
-alias lr='\ls -lR'          # Recursive ls
-alias ls='\ls -G --color=auto'
+alias ls='\gls -G --color=auto'
+alias ld='ls -ld'          # Show info about the directory
+alias lla='ls -lAF'        # Show hidden all files
+alias ll='ls -lF'          # Show long file information
+alias la='ls -AF'          # Show hidden files
+alias lx='ls -lXB'         # Sort by extension
+alias lk='ls -lSr'         # Sort by size, biggest last
+alias lc='ls -ltcr'        # Sort by and show change time, most recent last
+alias lu='ls -ltur'        # Sort by and show access time, most recent last
+alias lt='ls -ltr'         # Sort by date, most recent last
+alias lr='ls -lR'          # Recursive ls
 alias rm='rm -iv'
 alias cp='cp -iv'
 alias mv='mv -iv'
