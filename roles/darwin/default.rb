00a6af487cd41034d687bf13f9f6b86715a780f1
civitaspo <civitaspo@gmail.com>
2021-12-20 15:29:55 +0900

    Remove un-used cookbooks and dotfiles from roles/darwin/default.rb

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,20 +1,15 @@
 include_role 'base'
 include_cookbook 'homebrew' # install packages from .Brewfile
 
-include_cookbook 'google-japanese-ime'
 include_cookbook 'zsh'
 include_cookbook 'tmux'
 include_cookbook 'neovim'
-include_cookbook 'vscode-insiders'
-include_cookbook 'tig'
 include_cookbook 'scripts'
 include_cookbook 'sbin'
 include_cookbook 'bin'
 
 # packages which require only dotfiles.
 # installed by `brew cask`
-dotfile '.config/karabiner'
-dotfile '.config/alacritty'
 dotfile '.doc'
 dotfile '.pryrc'
 dotfile '.secret-env.sh'
