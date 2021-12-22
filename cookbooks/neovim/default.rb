03d6d8994c14d0a81c99d9042a0bf5add6634dfd
civitaspo <civitaspo@gmail.com>
2021-12-22 20:24:37 +0900

    Install neovim by package resource & Comment out python installer for neovim

--- cookbooks/neovim/default.rb
+++ cookbooks/neovim/default.rb
@@ -1,5 +1,6 @@
-# brew install neovim/neovim/neovim
 include_cookbook 'homebrew'
+
+package 'neovim'
 
 directory File.join(ENV['HOME'], '.config', 'nvim')
 
@@ -8,12 +9,12 @@ dotfile '.config/nvim/dein.toml'
 dotfile '.config/nvim/deinlazy.toml'
 dotfile '.config/nvim/plugins'
 
-execute 'env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.7.7' do
-  not_if "test -f #{File.join(ENV['HOME'], '.pyenv/versions/3.7.7/bin/python')}"
-end
-execute 'pyenv virtualenv 3.7.7 neovim' do
-  not_if "test -f #{File.join(ENV['HOME'], '.pyenv/versions/neovim/bin/python')}"
-end
-execute "#{File.join(ENV['HOME'], '.pyenv/versions/neovim/bin/pip')} install neovim" do
-  not_if "#{File.join(ENV['HOME'], '.pyenv/versions/neovim/bin/pip')} list | grep neovim >/dev/null"
-end
+# execute 'env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.7.7' do
+#   not_if "test -f #{File.join(ENV['HOME'], '.pyenv/versions/3.7.7/bin/python')}"
+# end
+# execute 'pyenv virtualenv 3.7.7 neovim' do
+#   not_if "test -f #{File.join(ENV['HOME'], '.pyenv/versions/neovim/bin/python')}"
+# end
+# execute "#{File.join(ENV['HOME'], '.pyenv/versions/neovim/bin/pip')} install neovim" do
+#   not_if "#{File.join(ENV['HOME'], '.pyenv/versions/neovim/bin/pip')} list | grep neovim >/dev/null"
+# end
