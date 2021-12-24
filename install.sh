8f066e639e1d2aaee911641d75fb98cd6e351c1b
civitaspo <civitaspo@gmail.com>
2021-12-24 20:34:05 +0900

    Follow mitamae log format

--- install.sh
+++ install.sh
@@ -19,28 +19,28 @@ MITAMAE_BIN_DIR="${MITAMAE_ROOT_DIR}/bin"
 __setup() {
     mkdir -p ${MITAMAE_BIN_DIR}
     if [ -f "${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}" ]; then
-        echo "[INFO] '${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}' already exists. Skip to download the binary."
-        echo "[INFO] Link: '${MITAMAE_CACHE}' -> '${MITAMAE_BIN_DIR}/mitamae'"
+        echo " INFO : '${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}' already exists. Skip to download the binary."
+        echo " INFO : Link: '${MITAMAE_CACHE}' -> '${MITAMAE_BIN_DIR}/mitamae'"
         ln -sf "${MITAMAE_CACHE}" "${MITAMAE_BIN_DIR}/mitamae"
         return 0
     fi
-    echo "[INFO] Download the binary from: ${MITAMAE_BIN_URL}"
+    echo " INFO : Download the binary from: ${MITAMAE_BIN_URL}"
     curl -o "${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz" -fsL "${MITAMAE_BIN_URL}"
-    echo "[INFO] Extract: ${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz"
+    echo " INFO : Extract: ${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz"
     tar xvzf "${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz"
-    echo "[INFO] Delete: ${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz"
+    echo " INFO : Delete: ${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz"
     rm "${MITAMAE_BIN_DIR}/${MITAMAE_BIN}.tar.gz"
-    echo "[INFO] Move and rename: '${MITAMAE_BIN}' -> '${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}'"
+    echo " INFO : Move and rename: '${MITAMAE_BIN}' -> '${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}'"
     mv "${MITAMAE_BIN}" "${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}"
     chmod +x "${MITAMAE_BIN_DIR}/${MITAMAE_CACHE}"
-    echo "[INFO] Link: '${MITAMAE_CACHE}' -> '${MITAMAE_BIN_DIR}/mitamae'"
+    echo " INFO : Link: '${MITAMAE_CACHE}' -> '${MITAMAE_BIN_DIR}/mitamae'"
     ln -sf "${MITAMAE_CACHE}" "${MITAMAE_BIN_DIR}/mitamae"
 }
 
 __main() {
     case "${MITAMAE_KERNEL_LC}" in
         "darwin")
-            echo '[INFO] Please input sudo password to avoid being asked for a password during provisioning.'
+            echo ' INFO : Please input sudo password to avoid being asked for a password during provisioning.'
             sudo -v && sudo -l mkdir &>/dev/null
             bin/mitamae local lib/recipe.rb "$@"
             ;;
